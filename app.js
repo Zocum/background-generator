fba3c21e3d676059ae26a6573f78cc177d1e6816
Zocum <hjclp18@gmail.com>
2022-05-31 10:19:20 +0200

    forgot js

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
+var css = document.querySelector("h3")
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
 
-btn.addEventListener('click', () => {
-    search.classList.toggle('active')
-    input.focus()
-})
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, "
+	+ color1.value 
+	+ ", "
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+
+function getRandomColor() {
+	var randomColor = Math.floor(Math.random()*16777215).toString(16);
+	document.body.style.background = "#" + randomColor;
+	color1.innerHTML = "#" + randomColor;
+	//color2.innerHTML = "#" + randomColor;
+}
+
+button.addEventListener("click", getRandomColor);
+color1.addEventListener("input", setGradient); //Alternative: comment this out & add 'oninput=setGradient' to input in HTML
+color2.addEventListener("input", setGradient);
